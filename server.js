91482d906fe19b281a3398dff9059a96c8123747
Devananth Kalarickal <devananthkalarickal85@gmail.com>
2024-12-04 20:06:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,45 +5,43 @@ const morgan = require("morgan");
 const cors = require("cors");
 const connectDB = require("./config/db");
 
-
-
-//dot config
+// Dotenv configuration
 dotenv.config();
 
-//mongodb connection
+// MongoDB connection
 connectDB();
 
-//rest object
+// Create Express app
 const app = express();
 
-//middlewares
+// Middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // Allow all origins
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+  })
+);
 app.use(morgan("dev"));
 
-
-
-//routes
-// 1 test route
+// Routes
 app.use("/api/v1/test", require("./routes/testRoutes"));
 app.use("/api/v1/auth", require("./routes/authRoutes"));
 app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
 app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 app.use("/api/v1/camp", require("./routes/campRoutes"));
-app.use("/api/v1/Ticket",require("./routes/ticketRoutes")); // Use ticket-related routes
-app.use('/api/v1/inventory', require('./routes/OrganRoutes'))
+app.use("/api/v1/Ticket", require("./routes/ticketRoutes"));
+app.use("/api/v1/inventory", require("./routes/OrganRoutes"));
 app.use("/api/v1/", require("./routes/UpdateRoutes"));
 
-
-
-//port
+// Port
 const PORT = process.env.PORT || 8080;
 
-//listen
+// Start server
 app.listen(PORT, () => {
   console.log(
-    `Node Server Running In ${process.env.DEV_MODE} ModeOn Port ${process.env.PORT}`
-      .bgBlue.white
+    `Node Server Running In ${process.env.DEV_MODE} Mode on Port ${PORT}`.bgBlue.white
   );
 });
